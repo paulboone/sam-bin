ac9569d5d1a289018661a09d11d4b93ae360c205
Paul Boone <narcissus.pong@gmail.com>
2016-03-03 11:15:07 -0500

    script for looking at short-term resource availability for shared jobs

---
+++ shared-sm-avail.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "---------     -----  -----  ------------  ------------  --------------"
+echo "SHARED"
+frank-avail shared 2>/dev/null | head -5 | tail -3
+echo "---------     -----  -----  ------------  ------------  --------------"
+echo "SHARED_LARGE"
+frank-avail shared_large 2>/dev/null | head -5 | tail -3
+echo "---------     -----  -----  ------------  ------------  --------------"
+echo "DIST_BIG"
+frank-avail dist_big 2>/dev/null | head -5 | tail -3
